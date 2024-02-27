57c8c1b1163b0d56764530fefad3b1be34c53030
fiyaznkp <fiyaznkp@gmail.com>
2024-02-27 14:32:11 +0530

    updated with toggle

--- mediaQuery.js
+++ mediaQuery.js
@@ -1,9 +1,22 @@
 // here we are doing mediaquery using js
 
 const header = document.querySelector('.show')
+const main = document.querySelector('.main')
 
-const deviceWidth = window.matchMedia(('max-width:500px'))
+main.addEventListener('click',()=>{
+    header.classList.toggle('hide')
+})
 
-if(deviceWidth.matches){
-    header.classList.add('hide')
-}
+// const deviceWidth = window.matchMedia('(max-width:500px)')
+
+
+// window.addEventListener('resize',resize)
+
+// function resize(){
+   
+//     if(deviceWidth.matches){
+//         header.classList.add('hide')
+//     }else{
+//         header.classList.remove('hide')
+//     }
+// }
