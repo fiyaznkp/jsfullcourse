0ffdd1f3021ed4d3ca1615158634fe612e8802ea
fiyaznkp <fiyaznkp@gmail.com>
2024-03-02 22:32:57 +0530

    dom excercises

---
+++ yestech.js
@@ -0,0 +1,35 @@
+// swicth case
+
+// let a = 1
+
+// switch(a){
+//     case 1:
+//     console.log('one')
+//     break;
+//     case 2:
+//     console.log ("two");
+//     break;
+//     case 3:
+//     console.log('three')
+//     break;
+//     default:
+//     console.log('invalid');
+//     break;
+// }
+// console.log(a)
+
+// let input = document.getElementsByName('text')[0]
+// let myh1 = document.getElementById('myh1')
+
+// function message(){
+//     myh1.innerText = "hello " + input.value
+// }
+// let myh1 = document.createElement('h1')
+
+// function add(){
+//     myh1.innerText="Hello"
+//     document.body.appendChild(myh1)
+// }
+// function remove(){
+//    myh1.remove()
+// }
