5f17e02114ea2c8f0a2f66c36f7945d8766e7e90
fiyaznkp <fiyaznkp@gmail.com>
2024-03-06 15:05:01 +0530

    arrow function , class ,inheritance in js

--- yestech.js
+++ yestech.js
@@ -1,76 +1,141 @@
-// swicth case
+// // swicth case
 
-// let a = 1
+// // let a = 1
 
-// switch(a){
-//     case 1:
-//     console.log('one')
-//     break;
-//     case 2:
-//     console.log ("two");
-//     break;
-//     case 3:
-//     console.log('three')
-//     break;
-//     default:
-//     console.log('invalid');
-//     break;
+// // switch(a){
+// //     case 1:
+// //     console.log('one')
+// //     break;
+// //     case 2:
+// //     console.log ("two");
+// //     break;
+// //     case 3:
+// //     console.log('three')
+// //     break;
+// //     default:
+// //     console.log('invalid');
+// //     break;
+// // }
+// // console.log(a)
+
+// // let input = document.getElementsByName('text')[0]
+// // let myh1 = document.getElementById('myh1')
+
+// // function message(){
+// //     myh1.innerText = "hello " + input.value
+// // }
+// // let myh1 = document.createElement('h1')
+
+// // function add(){
+// //     myh1.innerText="Hello"
+// //     document.body.appendChild(myh1)
+// // }
+// // function remove(){
+// //    myh1.remove()
+// // }
+// // let heading = document.getElementById('heading')
+// // let btn = document.getElementById('btn')
+
+// // btn.addEventListener('click',changeColour)
+// // heading.addEventListener('mouseover',changeBg)
+
+// // function changeColour(){
+// //     heading.style.color = 'red'
+// // }
+// // function changeBg(){
+// //     heading.style.backgroundColor = 'green'
+// // }
+
+// // oops in js
+
+// class employee {
+//     constructor (name,age,position,salary){
+//         this.name =name;
+//         this.age=age;
+//         this.position =position;
+//         this.salary=salary
+//     }
+//     getSalary(){
+//         console.log((`salary of ${this.name} is ${this.salary}`))
+//     }
 // }
-// console.log(a)
+// let employee1 = new employee('john',30,"Manager",40000)
+// let employee2 = new employee('jacob',32,'HR',32000)
+// employee1.getSalary()
+// employee2.getSalary()
+// console.log(employee1.name)
 
-// let input = document.getElementsByName('text')[0]
-// let myh1 = document.getElementById('myh1')
+// // inheritance in oops
 
-// function message(){
-//     myh1.innerText = "hello " + input.value
+// class Manager extends employee {
+
 // }
-// let myh1 = document.createElement('h1')
+// let Manager1 = new Manager('smith',34,'Manager',45000)
+// console.log(Manager1)
+// Manager1.getSalary()
+// console.log(Manager1.name)
 
-// function add(){
-//     myh1.innerText="Hello"
-//     document.body.appendChild(myh1)
+//default parameter in js 
+
+// function sum (a=10,b=20){
+// console.log(a+b)
 // }
-// function remove(){
-//    myh1.remove()
+// sum()  here the result is 30 if we give arguments then it will not take default values also if we give one argument and there are two parameters then it will take second value from the default parameter
+
+// arrow function 
+
+// let number = (num)=>{
+//     if(num>0){
+//         console.log('positive number')
+//     }else if(num<0){
+//         console.log('negative number')
+//     }else{
+//         console.log('zero')
+//     }
 // }
-// let heading = document.getElementById('heading')
-// let btn = document.getElementById('btn')
+// number(1)
+// number(-1)
+// number(0)
 
-// btn.addEventListener('click',changeColour)
-// heading.addEventListener('mouseover',changeBg)
+// class in js
 
-// function changeColour(){
-//     heading.style.color = 'red'
-// }
-// function changeBg(){
-//     heading.style.backgroundColor = 'green'
-// }
-
-// oops in js
-
-class employee {
-    constructor (name,age,position,salary){
-        this.name =name;
-        this.age=age;
-        this.position =position;
-        this.salary=salary
+class Bikes {
+    // constructor nethod
+    constructor(model,colour,price){
+        this.model=model;
+        this.colour=colour;
+        this.price=price
     }
-    getSalary(){
-        console.log((`salary of ${this.name} is ${this.salary}`))
+    // prototype method 
+    bikeDetails(){
+        console.log('Bike: ' + this.model)
+        console.log('Colour:' + this.colour)
+        console.log('price: ' + this.price)
+    }
+    showPrice(){
+        console.log('price of ' + this.model + ' is ' + this.price)
+    }
+    static message(){
+        console.log('its a static method')
     }
 }
-let employee1 = new employee('john',30,"Manager",40000)
-let employee2 = new employee('jacob',32,'HR',32000)
-employee1.getSalary()
-employee2.getSalary()
-console.log(employee1.name)
+let Bike1 = new Bikes('Suzuki Axus','silver',65000)
+let Bike2 = new Bikes('hero maestro','black',55000)
+let Bike3 = new Bikes('royal enfield bullet classic 350','army green',175000)
 
-// inheritance in oops
+// console.log(Bike1.model)
+// console.log(Bike2.colour)
+// console.log(Bike3)
 
-class Manager extends employee {
+// Bike1.bikeDetails()
+// Bike2.showPrice()
+
+// Bikes.message()
+// inheritance 
+
+class sportsBikes extends Bikes{
 
 }
-let Manager1 = new Manager('smith',34,'Manager',45000)
-console.log(Manager1)
-Manager1.getSalary()
-console.log(Manager1.name)
+let sportsBike1 = new sportsBikes('ktm duke 390','orange',200000)
+
+sportsBike1.bikeDetails()
